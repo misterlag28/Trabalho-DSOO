9ec43aae92debe6c7575276f43bbd7083ac73c15
Matheus Soares <168029695+misterlag28@users.noreply.github.com>
2026-06-05 17:41:07 -0300

    Update pagamento.py

--- TrabalhoDSO/Clínica/pagamentos/pagamento.py
+++ TrabalhoDSO/Clínica/pagamentos/pagamento.py
@@ -5,18 +5,18 @@ from datetime import date
 class Pagamento(ABC):
 
     def __init__(self, data: date, valor_pago: float):
+
+        if valor_pago <= 0:
+            raise ValueError("Valor pago deve ser maior que zero.")
+
         self.__data = data
         self.__valor_pago = valor_pago
 
-        # RELAÇÕES
         self.atendimento = None
         self.paciente = None
 
-    @abstractmethod
-    def tipo_pagamento(self):
-        pass
-
     # GETTERS
+
     def get_data(self):
         return self.__data
 
@@ -24,8 +24,31 @@ class Pagamento(ABC):
         return self.__valor_pago
 
     # SETTERS
+
     def set_data(self, data: date):
         self.__data = data
 
     def set_valor_pago(self, valor_pago: float):
+
+        if valor_pago <= 0:
+            raise ValueError("Valor inválido.")
+
         self.__valor_pago = valor_pago
+
+    @abstractmethod
+    def tipo_pagamento(self):
+        pass
+
+    def calcular_valor_restante(self):
+
+        if self.atendimento is None:
+            raise ValueError("Pagamento sem atendimento associado.")
+
+        return self.atendimento.get_valor() - self.__valor_pago
+
+    def validar(self):
+
+        if self.__valor_pago <= 0:
+            raise ValueError("Valor inválido.")
+
+        return True
