a28b6e5a8e7a11ae7acc3411c2ae9b56ecdd9e24
Matheus Soares <168029695+misterlag28@users.noreply.github.com>
2026-06-05 17:40:24 -0300

    Update cartao_credito.py

--- TrabalhoDSO/Clínica/pagamentos/cartao_credito.py
+++ TrabalhoDSO/Clínica/pagamentos/cartao_credito.py
@@ -1,25 +1,28 @@
 from Pagamento import Pagamento
-from datetime import date
-
 
 class CartaoCredito(Pagamento):
 
     def __init__(
         self,
-        data: date,
-        valor_pago: float,
+        data,
+        valor_pago,
         numero_cartao: str,
         bandeira: str
     ):
+
         super().__init__(data, valor_pago)
+
+        if numero_cartao.strip() == "":
+            raise ValueError("Número do cartão inválido.")
+
+        if bandeira.strip() == "":
+            raise ValueError("Bandeira inválida.")
 
         self.__numero_cartao = numero_cartao
         self.__bandeira = bandeira
 
-    def tipo_pagamento(self):
-        return "Cartão de Crédito"
-
     # GETTERS
+
     def get_numero_cartao(self):
         return self.__numero_cartao
 
@@ -27,8 +30,32 @@ class CartaoCredito(Pagamento):
         return self.__bandeira
 
     # SETTERS
+
     def set_numero_cartao(self, numero_cartao: str):
+
+        if numero_cartao.strip() == "":
+            raise ValueError("Número inválido.")
+
         self.__numero_cartao = numero_cartao
 
     def set_bandeira(self, bandeira: str):
+
+        if bandeira.strip() == "":
+            raise ValueError("Bandeira inválida.")
+
         self.__bandeira = bandeira
+
+    def tipo_pagamento(self):
+        return "Cartão de Crédito"
+
+    def validar(self):
+
+        super().validar()
+
+        if self.__numero_cartao.strip() == "":
+            raise ValueError("Número inválido.")
+
+        if self.__bandeira.strip() == "":
+            raise ValueError("Bandeira inválida.")
+
+        return True
