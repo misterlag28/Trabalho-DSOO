75b9776973c4b0beab4940dd4fddccaf87231549
Matheus Soares <168029695+misterlag28@users.noreply.github.com>
2026-06-05 17:56:57 -0300

    Update dinheiro.py
    Sistema de troco adicionado a classe Dinheiro.

--- TrabalhoDSO/Clínica/pagamentos/dinheiro.py
+++ TrabalhoDSO/Clínica/pagamentos/dinheiro.py
@@ -1,11 +1,46 @@
 from Pagamento import Pagamento
-from datetime import date
 
 
 class Dinheiro(Pagamento):
 
-    def __init__(self, data: date, valor_pago: float):
+    def __init__(
+        self,
+        data,
+        valor_pago: float,
+        valor_recebido: float
+    ):
+
         super().__init__(data, valor_pago)
+
+        if valor_recebido < valor_pago:
+            raise ValueError(
+                "Valor recebido menor que o valor do pagamento."
+            )
+
+        self.__valor_recebido = valor_recebido
+
+    # GETTER
+
+    def get_valor_recebido(self):
+        return self.__valor_recebido
+
+    # SETTER
+
+    def set_valor_recebido(self, valor_recebido):
+
+        if valor_recebido < self.get_valor_pago():
+            raise ValueError(
+                "Valor recebido insuficiente."
+            )
+
+        self.__valor_recebido = valor_recebido
+
+    def calcular_troco(self):
+
+        return (
+            self.__valor_recebido
+            - self.get_valor_pago()
+        )
 
     def tipo_pagamento(self):
         return "Dinheiro"
