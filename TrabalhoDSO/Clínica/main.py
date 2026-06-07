48e9fbdf5422a976b31a15e317795e1ebb94b36c
Matheus Soares <168029695+misterlag28@users.noreply.github.com>
2026-06-06 23:30:40 -0300

    Update main.py

--- TrabalhoDSO/Clínica/main.py
+++ TrabalhoDSO/Clínica/main.py
@@ -1,7 +1,4 @@
-from datetime import date, time
+from controladorPrincipal import ControladorPrincipal
 
-from modelos.paciente import Paciente
-from modelos.profissional_saude import ProfissionalSaude
-from modelos.clinica import Clinica
-from modelos.atendimento import Atendimento
-from enums.tipo_atendimento import TipoAtendimento
+controlador = ControladorPrincipal()
+controlador.iniciar()
